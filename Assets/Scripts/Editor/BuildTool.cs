54a4708d9579767886029bfedbb29a227b444b9c
XiangBoWen <subway001122>
2022-08-04 16:41:44 +0800

    Add ResourceLoad

--- Assets/Scripts/Editor/BuildTool.cs
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Linq;
 
 public class BuildTool : Editor  //继承子Editor的不会被打包打进去
 {
@@ -27,6 +28,10 @@ public class BuildTool : Editor  //继承子Editor的不会被打包打进去
     private static void Build(BuildTarget target)
     {
         List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
+
+
+        List<string> bundleInfos = new List<string>();  //文件信息列表
+
         string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath, "*", SearchOption.AllDirectories); //查找目录下的所有文件
         for (int i = 0; i < files.Length; i++)
         {
@@ -34,22 +39,31 @@ public class BuildTool : Editor  //继承子Editor的不会被打包打进去
             {
                 continue;
             }
-            Debug.LogFormat("file:" + files[i]);
+            //Debug.LogFormat("file:" + files[i]);
             AssetBundleBuild assetBundle = new AssetBundleBuild();
 
             string fileName = PathUtil.GetStandardPath(files[i]);
             string assetName = PathUtil.GetUnityPath(fileName);  //获取文件相对路径
 
             assetBundle.assetNames = new string[] { assetName };
-            Debug.LogFormat("assetNames: {0}", assetBundle.assetNames);
 
             string bundleName = files[i].Replace(PathUtil.BuildResourcesPath, "").ToLower(); //设置bundleName
+            bundleName = PathUtil.GetStandardPath(bundleName);
             bundleName = bundleName.Remove(0, 1);
-            Debug.LogFormat("bundleName: {0}",bundleName);
 
             assetBundle.assetBundleName = bundleName + ".ab";
-            Debug.LogFormat("assetBundleName: {0}", assetBundle.assetBundleName);
             assetBundleBuilds.Add(assetBundle);
+
+            //添加文件信息和依赖信息
+            List<string> dependenceInfo = GetDependence(assetName);
+            string bundleInfo = assetName + "|" + bundleName + ".ab";
+            Debug.LogFormat("bundleInfo: {0}", bundleInfo);
+            if (dependenceInfo.Count > 0)
+            {
+                bundleInfo = bundleInfo + "|" + string.Join("|", dependenceInfo);
+            }
+            Debug.LogFormat("bundleInfo: {0}", bundleInfo);
+            bundleInfos.Add(bundleInfo);
         }
 
         //保证一定有bundle的输出文件目录，且目录干净
@@ -59,7 +73,24 @@ public class BuildTool : Editor  //继承子Editor的不会被打包打进去
         }
         Directory.CreateDirectory(PathUtil.BundleOutPath);
 
-        Debug.LogFormat("BundleOutPath :{0}", PathUtil.BundleOutPath);
+        //Debug.LogFormat("BundleOutPath :{0}", PathUtil.BundleOutPath);
         BuildPipeline.BuildAssetBundles(PathUtil.BundleOutPath, assetBundleBuilds.ToArray(), BuildAssetBundleOptions.None, target);  //打包操作
+
+        File.WriteAllLines(PathUtil.BundleOutPath + "/" + AppConst.FileListName, bundleInfos);  //创建依赖资源文件并写入
+
+        AssetDatabase.Refresh();  //刷新unity目录
+    }
+
+    /// <summary>
+    /// 获取依赖文件列表
+    /// </summary>
+    /// <param name="curFile"></param>
+    /// <returns></returns>
+    static List<string> GetDependence(string curFile)
+    {
+        List<string> dependence = new List<string>();
+        string[] files = AssetDatabase.GetDependencies(curFile);
+        dependence = files.Where(file => !file.EndsWith(".cs") && !file.Equals(curFile)).ToList();
+        return dependence;
     }
 }
--- Assets/Scripts/Framwork/Util/PathUtil.cs
+++ Assets/Scripts/Framwork/Util/PathUtil.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PathUtil
 {
+
     //根目录
     public static readonly string AseetPath = Application.dataPath;
 
@@ -12,6 +13,12 @@ public class PathUtil
 
     //bundle输出目录
     public static readonly string BundleOutPath = Application.streamingAssetsPath;
+
+    //bundle资源路径
+    public static string BundleResourcePath
+    {
+        get { return Application.streamingAssetsPath; }
+    }
 
     /// <summary>
     /// 通过绝对路径获取Unity的相对路径
